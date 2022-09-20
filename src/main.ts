264ac3c63c2b6221be0271709d7b27910a9cc06e
Kysomaio <thoaiky1992@gmail.com>
2022-09-20 18:01:32 +0700

    fix: body-parser

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as bodyParser from 'body-parser';
 import { AppModule } from './app.module';
 import { AppExceptionFilter } from './libraries/http-exception.filter';
 import { config } from 'dotenv';
@@ -10,6 +11,8 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AppExceptionFilter());
   app.setGlobalPrefix('api');
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(process.env.PORT);
 }
 bootstrap();
